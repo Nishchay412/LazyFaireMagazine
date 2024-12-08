dcde35229b26929c52e5b4deb3bc43c47791fc95
Nishchay Ranjan <nishchayranjan878@gmail.com>
2024-12-08 15:18:49 -0700

    animation

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Footer } from "./Footer";
+import { motion } from "framer-motion";
 
 export function Hero() {
   return (
@@ -8,12 +9,27 @@ export function Hero() {
       <div className="bg-[url('/backgroundimage.png')] h-screen bg-cover bg-center">
         {/* Centered Front Page Magazine */}
         <div className="flex items-center justify-center h-full">
-        <img
-  src="/frontpage_magazine.png"
-  className="w-auto h-auto max-w-xs max-h-[75vh] object-contain shadow-lg py-2 mt-6"
-  alt="Front page magazine"
-/>
-
+          <motion.div
+            className="w-auto h-auto max-w-xs max-h-[75vh] object-contain rounded-lg shadow-lg py-2 mt-6"
+            initial={{ boxShadow: "0px 0px 0px rgba(255, 255, 255, 0)" }}
+            animate={{
+              boxShadow: [
+                "0px 0px 0px rgba(255, 255, 255, 0)",
+                "0px 0px 20px rgba(255, 255, 255, 1)",
+                "0px 0px 0px rgba(255, 255, 255, 0)",
+              ],
+            }}
+            transition={{
+              duration: 2,
+              repeat: Infinity,
+            }}
+          >
+            <img
+              src="/frontpage_magazine.png"
+              className="-auto h-auto max-w-xs max-h-[75vh] object-contain"
+              alt="Front page magazine"
+            />
+          </motion.div>
         </div>
       </div>
 
