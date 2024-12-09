93cd018a312b70e3d58c3ed25c99a98937964f23
Nishchay Ranjan <nishchayranjan878@gmail.com>
2024-12-09 14:38:10 -0700

    cc

--- src/components/teaminfo.jsx
+++ src/components/teaminfo.jsx
@@ -4,51 +4,52 @@ export const executives = [
     name: "Gaurav Dhir",
     email: "gdhir@ualberta.ca",
     faculty: "Business",
-    img: "gaurav_dhir_president.jpg"
+    img: "Gaurav Dhir_President.jpg"
   },
   {
     position: "VP Design",
     name: "Shanessa Fernandes",
     email: "shanessa@ualberta.ca",
     faculty: "Arts",
-    img: "shanessa_fernandes.jpg"
+    img: "shanessa.jpg"
   },
   {
     position: "VP External",
     name: "Jerick Nucum",
     email: "nucum@ualberta.ca",
     faculty: "Business",
-    img: "jerick_nucum_external.jpg"
+    img: "Jerick External.jpg"
   },
   {
     position: "VP Finance",
     name: "Mohit Makhija",
     email: "mmakhija@ualberta.ca",
     faculty: "Business",
-    img: "mohit_makhija_finance.jpg"
+    img: "mohit finance-01-01.jpg"
   },
   {
     position: "VP Marketing",
     name: "Zahrah Ismail",
     email: "zahrah1@ualberta.ca",
     faculty: "Business",
-    img: "zahrah_ismail_marketing.jpg"
+    img: "Zahrah Marketing.jpg"
   },
   {
     position: "VP Internal",
     name: "Lorenzo Contreras",
     email: "lcontre1@ualberta.ca",
     faculty: "Business",
-    img: "lorenzo_contreras_internal.jpg"
+    img: "internal.jpg"
   },
   {
     position: "VP Editorial",
     name: "Zoë Kemppi",
     email: "kemppi@ualberta.ca",
     faculty: "Business",
-    img: "zoe_kemppi_editorial.jpg"
+    img: "Zoe editorial.jpg"
   }
 ];
+
 
 
 
@@ -69,7 +70,7 @@ export const executives = [
     },
     {
       position: "Designer",
-      name: "Reinaissa Ulah",
+      name: "Reinaissa Ullah",
       email: "nucum@ualberta.ca",
       faculty: "Business",
       img : "renaissa.jpg"
@@ -90,7 +91,7 @@ export const executives = [
     },
     {
       position: "Designer",
-      name: "Ayomide Papoola",
+      name: "Ayomide Popoola",
       email: "lcontre1@ualberta.ca",
       faculty: "Business",
       img:"aypp.jpg"
