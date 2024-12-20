86c7a8643d30fb55c981809b37d9e592c5689734
Nishchay Ranjan <nishchayranjan878@gmail.com>
2024-12-20 04:40:42 -0700

    cc

--- src/readmagazine.jsx
+++ src/readmagazine.jsx
@@ -1,32 +1,23 @@
+
+
+
+
+
 import React from "react";
 
 export function Read_Magazine() {
-  const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-
   return (
     <div className="h-screen bg-[url('/backgroundimage.png')] bg-cover bg-center p-4 overflow-x-hidden">
       <h1 className="text-center text-xl md:text-2xl font-semibold text-white mb-4">
         LazyFaire Magazine
       </h1>
 
-      {isMobile ? (
-        <div className="text-center">
-          <a
-            href="https://online.flippingbook.com/view/675970383/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-white underline text-lg"
-          >
-            Open LazyFaire Magazine
-          </a>
-        </div>
-      ) : (
-        <div
-          className="relative mx-auto w-full max-w-5xl"
-          style={{ height: "80vh" }}
-        >
-          {/* Heyzine Flipbook Embed */}
-          <iframe
+      <div
+        className="relative mx-auto w-full max-w-5xl"
+        style={{ height: "80vh" }}
+      >
+        {/* Heyzine Flipbook Embed */}
+        <iframe
             allowFullScreen
             scrolling="no"
             className="fp-iframe"
@@ -38,8 +29,7 @@ export function Read_Magazine() {
             }}
             src="https://heyzine.com/flip-book/314ff53df2.html"
           ></iframe>
-        </div>
-      )}
+      </div>
 
       <footer className="text-center text-white text-sm mt-4">
         © 2024 LazyFaire Magazine
